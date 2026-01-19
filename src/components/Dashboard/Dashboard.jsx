720b294c349449bb00846225f81180cf1adc4269
Taafaadzwaa <tafadzwatsungi@gmail.com>
2026-01-19 11:58:01 +0200

    first commit

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from"react";
+import StatsGrid from "./StatsGrid";
+
+function Dashboard(){
+    return <div className="space-y-6 ">
+        {/*Stats grid */}
+    </div>
+}
+
+export default Dashboard;
